13994adf6e1a21867053ed207ef8d82346bcfd40
Juan Zurita <juan.zurita.60112@usalesiana.edu.bo>
2022-07-30 19:26:58 -0400

    updated time measure method

--- 01 - Binary Search Tree/lib/src/main/java/timeMeasure/TimeMeasure.java
+++ 01 - Binary Search Tree/lib/src/main/java/timeMeasure/TimeMeasure.java
@@ -2,17 +2,17 @@ package timeMeasure;
 
 public class TimeMeasure {
     public static void measure(Runnable runnable) {
-        long initTime = System.currentTimeMillis();
+        long initTime = System.nanoTime();
         runnable.run();
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime - initTime);
+        long endTime = System.nanoTime();
+        System.out.println(nanoToMillis(endTime - initTime));
     }
 
-    public static long millisToSeconds(long millis) {
-        return millis / 1000;
+    private static double nanoToMillis(long nanoTime) {
+        return roundWithToDecimals(nanoTime / 1000000.0);
     }
 
-    public static long millisToMinutes(long millis) {
-        return millisToSeconds(millis) / 60;
+    private static double roundWithToDecimals(double value) {
+        return Math.round(value * 100.0) / 100.0;
     }
 }
